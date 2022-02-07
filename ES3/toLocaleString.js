f43b360dea945ae25196d3e379271fa175c50550
Jander Teodoro <79011974+janderteodoro@users.noreply.github.com>
2022-02-07 12:55:20 -0300

    finishing ES3 methods

---
+++ ES3/toLocaleString.js
@@ -0,0 +1,5 @@
+// simplesmente a versão localizada do to Strings
+
+const a = [1, 2, 3]; 
+const b = a.toLocaleString(); 
+ 
---
+++ ES3/toString.js
@@ -0,0 +1,8 @@
+/*
+Assim como qualquer outro objeto em JS, um array tambem tem u método que transforma seus elementos em string 
+
+ele pega cada um e coloca dentro de um única string, separando por vírgula
+*/
+
+const a = [1,'Carro', 2, 3];
+console.log(a.toString()) // = 1,Carro, 2, 3 do tipo String. Faz basicamente o que o join faz sem argumentos
